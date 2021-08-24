756203eb6422f104e7ba5d01fe0744182d5878c5
TarasiukDima <dimatarasiuk91@gmail.com>
2021-08-24 20:52:13 +0300

    fix: add normal infinity slide

--- main.js
+++ main.js
@@ -9,26 +9,48 @@ document.addEventListener('DOMContentLoaded', () => {
             this.rigthSlider = this.slider.querySelector('.right__slider');
             this.activeSlide = 0;
             this.length = this.leftSlider.querySelectorAll('.slider__item').length;
-            this.startX = 0;
             this.startY = 0;
-            this.endX = 0;
             this.endY = 0;
+            this.canChange = false;
+            this.heightContainer = this.slider.getBoundingClientRect().height;
 
             this.init = this.init.bind(this);
             this._listenerBtnsParrent = this._listenerBtnsParrent.bind(this);
             this._listenerKeyPress = this._listenerKeyPress.bind(this);
             this._listenerMouseSwipe = this._listenerMouseSwipe.bind(this);
+            this._listenerMousewheel = this._listenerMousewheel.bind(this);
+            this._startPosition = this._startPosition.bind(this);
+            this._endPosition = this._endPosition.bind(this);
             this._swipeDirection = this._swipeDirection.bind(this);
             this._changeSlide = this._changeSlide.bind(this);
+            this._cloneSlide = this._cloneSlide.bind(this);
+            this._startSlider = this._startSlider.bind(this);
+            this._removeTransition = this._removeTransition.bind(this);
         }
 
         // init simple slider
         init() {
+            this._listenerBtnsParrent();
+            this._listenerKeyPress();
+            this._listenerMousewheel();
+            this._listenerMouseSwipe();
+            this._startSlider();
+
+            this.leftSlider.addEventListener('transitionend', this._removeTransition);
+            this.rigthSlider.addEventListener('transitionend', this._removeTransition);
+        }
+
+        //start slider
+        _startSlider() {
+            this.activeSlide = 1;
+            this._cloneSlide(this.leftSlider, 'next');
+            this._cloneSlide(this.rigthSlider);
+            this.length = this.leftSlider.querySelectorAll('.slider__item').length;
+
             this.leftSlider.style.top = `-${ 100 * (this.length - 1) }vh`;
 
-            this._listenerBtnsParrent()
-            this._listenerKeyPress()
-            this._listenerMouseSwipe()
+            this.leftSlider.style.transform = `translateY(${this.activeSlide * this.heightContainer}px)`;
+            this.rigthSlider.style.transform = `translateY(-${this.activeSlide * this.heightContainer}px)`;
         }
 
         // listener for slider btn
@@ -56,65 +78,106 @@ document.addEventListener('DOMContentLoaded', () => {
             })
         }
 
+        // listener for mousewheel
+        _listenerMousewheel() {
+            document.addEventListener('mousewheel', (e) => {
+                if (e.deltaY > 0) {
+                    this._changeSlide('prev');
+                } else {
+                    this._changeSlide('next');
+                }
+            })
+        }
+
         // listener for swipe
         _listenerMouseSwipe() {
-            this.slider.addEventListener('mousedown', (e) => {
-                this.startX = e.clientX;
-                this.startY = e.clientY;
-            });
+            this.slider.addEventListener('mousedown', this._startPosition);
+            this.slider.addEventListener('touchstart', this._startPosition);
 
-            this.slider.addEventListener('mousemove', (e) => {
-                this.endX = e.clientX;
-                this.endY = e.clientY;
-            });
+            this.slider.addEventListener('mousemove', this._endPosition);
+            this.slider.addEventListener('touchmove', this._endPosition);
 
             this.slider.addEventListener('mouseup', this._swipeDirection);
+            this.slider.addEventListener('touchend', this._swipeDirection);
+        }
+
+        // defenition start position touch or mosedown
+        _startPosition(e) {
+            this.startY = e.clientY || e.changedTouches[0].pageY;
+        }
+
+        // defenition end position touch or mosedown
+        _endPosition(e) {
+            this.endY = e.clientY || e.changedTouches[0].pageY;
         }
 
         // defenition swipe direction
         _swipeDirection() {
-            const xDiff = this.endX - this.startX;
             const yDiff = this.endY - this.startY;
 
-            if (xDiff === 0 || yDiff === 0) return;
+            if (yDiff === 0) return;
 
-            if (Math.abs(xDiff) < Math.abs(yDiff)) {
-                if (yDiff > 0) {
-                    //down
-                    this._changeSlide('prev');
-                } else {
-                    //up
-                    this._changeSlide('next');
-                }
+            if (yDiff > 0) {
+                //down
+                this._changeSlide('prev');
+            } else {
+                //up
+                this._changeSlide('next');
             }
 
             // reset values
-            this.startX = 0;
             this.startY = 0;
-            this.endX = 0;
             this.endY = 0;
         }
 
         // change active slide
         _changeSlide(param = 'next') {
-            let heightContainer = this.slider.getBoundingClientRect().height;
+            if (this.canChange) return;
+
+            this.canChange = true;
+            this.slider.classList.add('animation');
 
             if (param === 'prev') {
                 this.activeSlide--;
-                this.activeSlide < 0
-                    ? this.activeSlide = this.length - 1
-                    : null;
-
             } else if (param === 'next') {
                 this.activeSlide++;
-
-                this.activeSlide > this.length - 1
-                    ? this.activeSlide = 0
-                    : null;
             }
 
-            this.leftSlider.style.transform = `translateY(${this.activeSlide * heightContainer}px)`;
-            this.rigthSlider.style.transform = `translateY(-${this.activeSlide * heightContainer}px)`;
+            this.leftSlider.style.transform = `translateY(${this.activeSlide * this.heightContainer}px)`;
+            this.rigthSlider.style.transform = `translateY(-${this.activeSlide * this.heightContainer}px)`;
+        }
+
+        // remove transition class from slider and check active index
+        _removeTransition() {
+            this.slider.classList.remove('animation');
+            this.canChange = false;
+
+            if (this.activeSlide > this.length - 2) {
+                this.activeSlide = 1;
+            }
+            if (this.activeSlide == 0) {
+                this.activeSlide = this.length - 2
+            };
+
+            this.leftSlider.style.transform = `translateY(${this.activeSlide * this.heightContainer}px)`;
+            this.rigthSlider.style.transform = `translateY(-${this.activeSlide * this.heightContainer}px)`;
+
+        }
+
+        // add last slide on start, and first slide on end
+        _cloneSlide(sliderVar, direction = 'next') {
+            const firstSlide = sliderVar.querySelectorAll('.slider__item')[0];
+            const lastSlide = sliderVar.querySelectorAll('.slider__item')[this.length - 1];
+            const cloneFirstSlide = firstSlide.cloneNode(true);
+            const cloneLastSlide = lastSlide.cloneNode(true);
+
+            if (direction === 'next') {
+                sliderVar.appendChild(cloneFirstSlide);
+                sliderVar.insertBefore(cloneLastSlide, firstSlide);
+            } else {
+                sliderVar.appendChild(cloneLastSlide);
+                sliderVar.insertBefore(cloneFirstSlide, firstSlide);
+            }
         }
     }
 
